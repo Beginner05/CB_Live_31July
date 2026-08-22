640362a494568bd78f49347a4cfe8c651faa0604
Hitesh Malhotra <hiteshmalhotra0018@gmail.com>
2026-08-22 14:48:35 +0530

    lec9

---
+++ Lec9/Krotate.java
@@ -0,0 +1,10 @@
+package Lec9;
+
+public class Krotate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lec9/Leetcode_238.java
@@ -0,0 +1,10 @@
+package Lec9;
+
+public class Leetcode_238 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
